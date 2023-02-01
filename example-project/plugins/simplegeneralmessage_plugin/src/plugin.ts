f9a9128bb424a11aa1bb0ef0a9e75004232ee3b5
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-01-31 19:14:11 -0600

    reverted

--- example-project/plugins/simplegeneralmessage_plugin/src/plugin.ts
+++ example-project/plugins/simplegeneralmessage_plugin/src/plugin.ts
@@ -119,10 +119,11 @@ export class DummyPlugin implements Plugin<WorkflowPayload> {
 
     // Here we are parsing the payload so that we can send it to the right recipient
     const hexPayload = parsed.payload.toString("hex");
-    let [recipient, destID, message] = ethers.utils.defaultAbiCoder.decode(["bytes32", "uint16", "string"], "0x" + hexPayload);
+    let [recipient, destID, sender, message] = ethers.utils.defaultAbiCoder.decode(["bytes32", "uint16", "bytes32", "string"], "0x" + hexPayload);
     recipient = this.formatAddress(recipient);
+    sender = this.formatAddress(sender);
     const destChainID = destID as ChainId;
-    this.logger.info(`VAA: sent "${message}" to ${recipient} on chain ${destID}.`);
+    this.logger.info(`VAA: ${sender} sent "${message}" to ${recipient} on chain ${destID}.`);
 
     // Execution logic
     if (isEVMChain(destChainID)) {
